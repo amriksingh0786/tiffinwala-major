265b3f03c4c80e64b628561d4c7dbaacd5fcb7ec
Amrik Singh Khalsa <singhamrikkhalsa@gmail.com>
2020-12-19 11:52:54 +0530

    rm done

---
+++ build/precache-manifest.fa4fdae714be46870cf16f02bb8fe5aa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adc562a516624123b63a8e42333e3fb4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d5350fd5da347207ae6",
+    "url": "/static/css/main.55071acd.chunk.css"
+  },
+  {
+    "revision": "2897aea822f826696f8d",
+    "url": "/static/js/2.77c395d7.chunk.js"
+  },
+  {
+    "revision": "0135287500bd9d64203d07b4f33f971f",
+    "url": "/static/js/2.77c395d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d5350fd5da347207ae6",
+    "url": "/static/js/main.4e1f2ad2.chunk.js"
+  },
+  {
+    "revision": "3bdf5355cb9b9ef46672",
+    "url": "/static/js/runtime-main.de31c957.js"
+  },
+  {
+    "revision": "09e6f5e77390c30cbf9590bd0022c883",
+    "url": "/static/media/shopping-bag.09e6f5e7.svg"
+  },
+  {
+    "revision": "169a504a63b8ba1f0579c9058b701219",
+    "url": "/static/media/tiffin.169a504a.png"
+  }
+]);
